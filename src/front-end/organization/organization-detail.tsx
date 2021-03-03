4b4c70bd8376050f3628c018c2310c8b4a6a3404
Josep Seto <jsetojseto@gmail.com>
2021-03-04 00:39:33 +0100

    filter repos

--- src/front-end/organization/organization-detail.tsx
+++ src/front-end/organization/organization-detail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RepositoryPanel } from '../repository/repository-panel';
 import { Organization } from './organization';
 
@@ -8,18 +8,33 @@ interface OrganizationDetailProps {
 
 export function OrganizationDetails( props: OrganizationDetailProps ) {
 	const { organization } = props
+	const [ repoFilter, setRepoFilter ] = useState('')
+
 	return (
 		<div className="organization-details">
+			
 			<img src={ organization?.avatar }/>
 			<h2>{ organization?.name }</h2>
 			<p>{ organization?.description }</p>
+
+			{ organization?.repositories &&
+				<input 
+					placeholder="Filter repositories by name"
+					onChange={ event => setRepoFilter( event.target.value ) }
+					value={ repoFilter }
+				/>
+			}
+
 			<div className="repository-list">
 				{
-					organization?.repositories.map( repo => (
+					organization?.repositories
+						.filter( repo => repo.name.includes( repoFilter ) )
+						.map( repo => (
 							<RepositoryPanel key={ repo.fullName } repository={ repo } />
 						))
 				}
 			</div>
+	
 		</div>
 	)
 } 
